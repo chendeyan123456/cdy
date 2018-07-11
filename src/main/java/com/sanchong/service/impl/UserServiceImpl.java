47094305bd05d8405032683e2faac48f0c8e196f
chendeyan <2546745062@qq.com>
2018-07-11 21:08:10 +0800

    20180711

--- src/main/java/com/sanchong/service/impl/UserServiceImpl.java
+++ src/main/java/com/sanchong/service/impl/UserServiceImpl.java
@@ -24,7 +24,7 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public void updateByPrimaryKeySelective(UUser uuser) {
 		// TODO Auto-generated method stub
-		uuserdao.
+		//uuserdao.
 	}
 	
 }
